4677a3ca78b810ec247cc55191d9a98ac95a1a15
tchtinku <59766489+tchtinku@users.noreply.github.com>
2020-03-25 21:17:18 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.scrollingtext;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * This app displays a scrollable TextView (a magazine article).
+ * All changes were made to the layout. No code changes.
+ * The code below is part of the Empty Activity template.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
